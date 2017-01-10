79e330bfc54f5e6b40a6da10c169af4cb285429a
Your Name <sheel.bedi@gmail.com>
2017-01-09 20:19:06 -0600

    character sum

---
+++ Character Sum/Character Sum.js
@@ -0,0 +1,13 @@
+// Character Sum
+// Given a string of arbitrary size, convert each character into its integer equivalent and sum the entirety.
+
+// Examples
+// Input	Output
+// str:
+// "123"	6
+// str:
+// "0101"	2
+// str:
+// "so cool!!1!"	1
+
+
