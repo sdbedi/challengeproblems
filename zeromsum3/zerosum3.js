8171ec2485b90c9b59f04a82b49af000ced83476
Your Name <sheel.bedi@gmail.com>
2017-02-09 16:09:11 -0600

    addeddescription

--- zeromsum3/zerosum3.js
+++ zeromsum3/zerosum3.js
@@ -1,4 +1,17 @@
+// Given an array of negative/positive integers, return true if there exists three numbers whose sum is zero.
 
+// Examples
+// Input	Output
+// numbers:
+// [ 1, 3, 2, -3 ]	true
+// numbers:
+// [ 0, 2, -2 ]	true
+// numbers:
+// [ 5, 7, 2, 9 ]	false
+// numbers:
+// [ 1, 1 ]	false
+// numbers:
+// [ 6, 9, 7, 5, 2, 4, 6, 8, 5, 5, -7, 6, 5, -7, -9, -6, -4, 7, 4, -5, -5, 4, 2, 1 ]	true
 
 
 
