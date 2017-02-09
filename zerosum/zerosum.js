a9681c1cf977ee8e80fb10788d4bbbe8294868c4
Your Name <sheel.bedi@gmail.com>
2017-02-09 17:53:33 -0600

    addeddescription

--- zerosum/zerosum.js
+++ zerosum/zerosum.js
@@ -1,4 +1,13 @@
 
+// Zero Sum
+// Given an array of negative/positive integers, return true if there exists two numbers whose sum is zero.
+
+// Examples
+// Input	Output
+// numbers:
+// [ 1, 3, 2, -3 ]	true
+// numbers:
+// [ 5, 7, 2, 9 ]	false
 
 function zeroSum (numbers) {
   var answer = false
