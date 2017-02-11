a1fcca67fc8a7393f66d8e1c0c45a3994de8e2d8
Your Name <sheel.bedi@gmail.com>
2017-02-10 22:12:53 -0600

    addedprompt

---
+++ wordswithinwords/wordswithin.js
@@ -0,0 +1,8 @@
+
+// Words within Words
+// Given an array of unique words, find the word that contains the greatest number of other words. A word must be at least two letters long.
+
+// Examples
+// Input	Output
+// wordList:
+// [ "gray", "grays", "ray", "rays", "strays" ]	"grays"
