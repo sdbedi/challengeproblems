b577696049d477f553e8781a38705222c6971e70
Your Name <sheel.bedi@gmail.com>
2017-02-10 22:14:27 -0600

    addedsolution

--- wordswithinwords/wordswithin.js
+++ wordswithinwords/wordswithin.js
@@ -6,3 +6,22 @@
 // Input	Output
 // wordList:
 // [ "gray", "grays", "ray", "rays", "strays" ]	"grays"
+
+function nestedWordCount (wordList) {
+  var currentWinner = '';
+  var currentWinnerCount = 0;
+  for (var i = 0; i < wordList.length; i++) {
+    var thisCount = 0;
+    for (var k = 0; k < wordList.length; k++) {
+      if (wordList[i].includes(wordList[k])) {
+        thisCount++;
+      }
+    }
+    if (thisCount > currentWinnerCount) {
+      currentWinner = wordList[i];
+      currentWinnerCount = thisCount;
+    }
+  }
+
+  return currentWinner;
+}
