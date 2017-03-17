8a85ce72ce62fa5e90b64a7157a4d189f8eead07
Your Name <sheel.bedi@gmail.com>
2017-03-17 11:17:18 -0500

    added iterative solution

--- squarerange/squarerange.js
+++ squarerange/squarerange.js
@@ -3,3 +3,15 @@
 //5 -16: 2
 
 //Simple, iterative solution:
+
+function solution(A, B) {
+    var isSquare = function (n) {
+      return n > 0 && Math.sqrt(n) % 1 === 0;
+    };
+    var counter = 0 
+    for (var i = A; i < B + 1; i ++) {
+      var test = isSquare(i)
+      if (test === true) {counter ++}
+    }
+    console.log(counter)
+}
