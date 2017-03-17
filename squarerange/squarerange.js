c3dddb63c5a9472a74996c12abf8415ceea339f5
Your Name <sheel.bedi@gmail.com>
2017-03-17 11:37:59 -0500

    added description of efficient solution

--- squarerange/squarerange.js
+++ squarerange/squarerange.js
@@ -15,3 +15,19 @@ function solution(A, B) {
     }
     console.log(counter)
 }
+
+//Better, more efficient way. Interate from square root of a to square root of b inclusive:
+// floor(sqrt(b)) - ceil(sqrt(a)) + 1
+
+// We take floor of sqrt(b) because we need to consider 
+// numbers before b.
+
+// We take ceil of sqrt(a) because we need to consider 
+// numbers after a.
+
+
+// For example, let b = 24, a = 8.  floor(sqrt(b)) = 4, 
+// ceil(sqrt(a)) = 3.  And number of squares is 4 - 3 + 1
+// = 2. The two numbers are 9 and 16.
+
+
