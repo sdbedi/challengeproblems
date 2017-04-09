a8e092afa50ce2d7955b9adbccc8200d0987bce5
Your Name <sheel.bedi@gmail.com>
2017-04-09 15:46:58 -0500

    added parameters

---
+++ Conditionals/Conditionals.js
@@ -0,0 +1,26 @@
+// Conditionals
+// Fill in your function to take the age variable, and return “Welcome” if they are age appropriate (21 or over), “Go Away” if they are too young, and “Stop Playing Around” if they give you and age above 100 or below 1. Remember the format of a basic if statement is as follows:
+
+// if(Conditional Statement){
+
+//   /*code in here*/
+
+// /*This else if statement is optional*/
+// }else if(/*Conditional Statement*/){
+
+//   /*code in here*/
+
+// /*This else if statement is optional*/
+// }else{
+
+//   /*code in here*/
+
+// }
+// Examples
+// Input	Output
+// age:
+// 21	"Welcome"
+// age:
+// 5	"Go Away"
+// age:
+// 150	"Stop Playing Around"
